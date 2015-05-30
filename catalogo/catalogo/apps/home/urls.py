0a09d0603c49e2ad9eb4f150fa3976d817200768
loremondragon <dfmera6@misena.edu.co>
2015-05-30 11:09:44 -0500

    catalogo
    primer commit

---
+++ catalogo/catalogo/apps/home/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns,url
+
+
+urlpatterns = patterns('catalogo.apps.home.views', 
+	url(r'^about/$','about_view', name = 'vista_about'),
+	url(r'^contacto/$','contacto_view', name = 'vista_contacto'),
+	url(r'^$','index_view', name = 'vista_index'),
+	url(r'^marcas/$','marcas_view', name = 'vista_marcas'),
+	url(r'^categorias/$','categorias_view', name = 'vista_categorias'),
+	url(r'^producto/(?P<id_prod>.*)/$','single_product_view', name = 'vista_single_producto'),
+	url(r'^productos/page/(?P<pagina>.*)/$', 'productos_view', name= 'vista_productos'),
+	url(r'^login/$', 'login_view', name = 'vista_login'),
+	url(r'^logout/$', 'logout_view', name = 'vista_logout'),
+	url(r'^registro/$','register_view', name = 'vista_registro'),
+
+
+)
