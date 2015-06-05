72cf329eddd4eddb4aec4549ec57424456eb991f
loremondragon <dfmera6@misena.edu.co>
2015-06-04 21:08:33 -0500

    5
    1

--- catalogo/catalogo/settings.py
+++ catalogo/catalogo/settings.py
@@ -24,7 +24,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['senademo.herokuapp.com']
+ALLOWED_HOSTS = ['senademo1.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -51,7 +51,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'media/'))
+MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'static/'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
