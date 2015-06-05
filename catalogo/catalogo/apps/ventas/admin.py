7737ab2daec5832cdac28b41cd63c5dee4ba7e8c
loremondragon <dfmera6@misena.edu.co>
2015-06-04 20:05:03 -0500

    q
    q

--- catalogo/catalogo/apps/ventas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from catalogo.apps.ventas.models import Marca, Producto, Categoria
-
-admin.site.register(Marca)
-admin.site.register(Producto)
-admin.site.register(Categoria)
